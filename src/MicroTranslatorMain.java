4da319845263ca8f43722651a643973924472cb6
evilmurmurlfeur <evilmurmurfleur@gmail.com>
2017-05-21 12:43:45 +0400

    Как работает метод clear()?

--- src/MicroTranslatorMain.java
+++ src/MicroTranslatorMain.java
@@ -16,6 +16,8 @@ public class MicroTranslatorMain {
        String add_eng_word;
        String add_rus_word="";
        ArrayList<String> add_rus_words = new ArrayList<>();
+       
+       dictionary_on.userWordsCollection=dictionary_main;
       
        
        while(running){
@@ -25,21 +27,24 @@ public class MicroTranslatorMain {
        else {
            switch(user_input){
                case ":add":
+                   
+                   add_rus_word="";
                    printAndWrite.output("Английское слово: ");
                     add_eng_word=printAndWrite.input();
-                    while(!(add_rus_word.compareTo("end")==0)){
+                    while(!(add_rus_word.compareTo(":end")==0)){
                     printAndWrite.output("Вариант перевода: ");
                     add_rus_word=printAndWrite.input();
-                    printAndWrite.output(add_rus_word);
-                    add_rus_words.add(add_rus_word);
+                    if (!(add_rus_word.compareTo(":end")==0)){ 
+                    add_rus_words.add(add_rus_word);}
                     }
                     dictionary_on.addTraslate(add_eng_word,add_rus_words);
+                    add_rus_words.clear(); 
                     break;
            }
-       if (!(dictionary_main.containsKey(user_input)))
+       if (!(dictionary_on.userWordsCollection.containsKey(user_input)))
        {printAndWrite.output("Такого слова нет в словаре ");}
        else{
-       printAndWrite.output(dictionary_main.get(user_input));    
+       printAndWrite.output(dictionary_on.userWordsCollection.get(user_input));    
        }
        }
        }
